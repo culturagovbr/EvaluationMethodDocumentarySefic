52c38a5d806cac8135fb06b02835c03b806d1d00
Pedro Philipe Alves de Oliveira <pedro.oliveira2@cultura.gov.br>
2018-01-03 11:46:46 -0200

    Atualização de tipologias

--- tipologia-oportunidades.php
+++ tipologia-oportunidades.php
@@ -4,9 +4,6 @@ return array(
     "Análise Fiscal e Financeira" => array(
         "Análise fiscal e financeira"),
 
-    "Medidas de Acessibilidade a Produtos e Bens Culturais" => array(
-        "Medidas de acessibilidade a produtos e bens culturais"),
-
     "Artes Cênicas" => array(
         "Circo",
         "Dança",
@@ -17,11 +14,11 @@ return array(
         "Desfile de escola de samba ou festivos de caráter musical e cênico que tenham relação com festividades regionais, com confecções de fantasias, adereços ou material cenográficos",
         "Ações de capacitação e treinamento de pessoal",
         "Teatro musical, quando sua encenação se estabelece por meio de dramaturgia, compreendendo danças e canções",
-        "Construção e manutenção de salas de teatro ou centros culturais comunitários em municípios com menos de 100.000 (cem mil) habitantes."),
+        "Construção e manutenção de salas de teatro ou centros culturais comunitários em municípios com menos de 100.000 (cem mil) habitantes"),
 
     "Audiovisual" => array(
-        "Produção audiovisual de curta e média metragem, incluindo rádios e TVs educativas e culturais",
-        "Difusão de acervo audiovisual nos diversos meios e suportes",
+        "Produção de conteúdo audiovisual de curta e média metragem, incluindo rádios e TVs educativas e culturais",
+        "Difusão de acervo e conteúdo audiovisual nos diversos meios e suportes",
         "Restauração e preservação de acervos audiovisuais",
         "Doação de acervos audiovisuais para cinematecas",
         "Ações de capacitação e treinamento de pessoal",
@@ -31,18 +28,7 @@ return array(
         "Jogos eletrônicos de caráter educativo e cultural",
         "Projetos audiovisuais transmidiáticos",
         "Produção de websérie",
-        "Construção e manutenção de salas de cinema que poderão funcionar também como centros culturais comunitários em municípios com menos de 100.000 (cem mil) mil habitantes"),
-
-    "Humanidades" => array(
-        "Livros ou obras de referência, impressos ou eletrônicos, de valor artístico, literário ou humanístico",
-        "Manutenção, preservação ou restauração de acervos bibliográficos e arquivísticos compreendidos por livros ou obras de referência, impressos ou eletrônicos, de valor artístico, literário ou humanístico",
-        "Eventos literários e ações educativo-culturais voltados para a promoção do livro e da criação literária, e para o incentivo à leitura",
-        "Doação ou aquisição de acervos para bibliotecas públicas, museus, arquivos públicos, cinematecas",
-        "Ações de capacitação, treinamento de pessoal, oficinas e aquisição de equipamentos, que tenham como finalidade a manutenção de acervos de bibliotecas públicas, museus, arquivos públicos e cinematecas",
-        "Periódicos e outras publicações",
-        "Festival literário",
-        "Ações de formação e capacitação em geral incluindo gestão e empreendedorismo cultural",
-        "Construção de bibliotecas desde que esteja prevista a implantação de espaço destinado a apresentações de teatro, exibição de filmes e outras atividades culturais em municípios com menos de 100.000 (cem mil) habitantes"),
+        "Construção e manutenção de salas de cinema que poderão funcionar também como centros culturais comunitários em municípios com menos de 100.000 (cem mil) habitantes"),
 
     "Música" => array(
         "Erudita",
@@ -53,21 +39,21 @@ return array(
 
     "Artes Visuais" => array(
         "Exposição de artes visuais que possua em sua concepção tratamento artístico e curatorial, em quaisquer suportes abrangendo as seguintes categorias: pintura, desenho, gravura, fotografia, escultura, objeto, grafite, instalação, performances, vídeo-arte, artes digitais, arte eletrônica, design, arquitetura, moda, arte cibernética e artes gráficas, que poderão se organizar sob a forma de exposições, feiras, festivais, mostras, circuitos artísticos",
-        "Ações de capacitação e treinamento de pessoal que resultem em exposição de artes visuais"),
+        "Ações educativo-culturais, inclusive seminários, oficinas, e palestras, assim como ações de capacitação e treinamento de pessoal que visem a formação e o fomento em artes visuais"),
 
     "Patrimônio Cultural Material e Imaterial" => array(
         "Doações ou aquisições de acervos culturais em geral para arquivos públicos e instituições culturais",
         "Preservação, restauração, conservação, salvaguarda, identificação, registro, educação patrimonial e acervos do patrimônio cultural material e imaterial",
         "Ações de documentação ou digitalização de acervo bibliográfico e arquivístico; pesquisa; sistematização de informação",
-        "Preservação, restauração, manutenção, readequação ou revitalização de equipamentos culturais ou edificações destinadas a preservação de patrimônio cultural",
+        "Preservação, restauração, manutenção, readequação ou revitalização de equipamentos culturais ou edificações destinadas à preservação de patrimônio cultural",
         "Ações de segurança para preservação de patrimônio cultural ou de acervos",
         "Ações educativo-culturais, inclusive seminários, oficinas e palestras, visando a preservação do patrimônio material, imaterial ou de acervos de valor cultural",
         "Treinamento de pessoal ou aquisição de equipamentos para manutenção de acervos, arquivos públicos e instituições congêneres",
-        "Elaboração de projetos de arquitetura e urbanismo",
+        "Elaboração de projetos de restauro (arquitetura e complementares) destinados à preservação de bens culturais materiais tombados pelos poderes públicos, federal, estadual, municipal ou distrital",
         "Manutenção de equipamentos culturais em geral",
         "Gastronomia de valor cultural (típica brasileira)",
         "Artesanato de valor cultural (origem tradicional)",
-        "Arquitetura de (reconhecido) valor cultural"),
+        "Arquitetura de reconhecido valor cultural"),
         
     "Museus e Memória" => array(
         "Doação ou aquisição de acervos para museus e instituições de preservação da memória",
@@ -80,5 +66,17 @@ return array(
         "Ação educativo-cultural, inclusive seminários, congressos, palestras",
         "Criação e implantação (projetos, construção, restauração e reforma)",
         "Ações de capacitação e treinamento de pessoal",
-        "Aquisição de equipamentos para a preservação e manutenção de acervos")
+        "Aquisição de equipamentos para a preservação e manutenção de acervos"),
+
+    "Humanidades" => array(
+        "Livros ou obras de referência, impressos ou eletrônicos, de valor artístico, literário ou humanístico",
+        "Manutenção, preservação ou restauração de acervos bibliográficos e arquivísticos compreendidos por livros ou obras de referência, impressos ou eletrônicos, de valor artístico, literário ou humanístico",
+        "Eventos literários e ações educativo-culturais voltados para a promoção do livro e da criação literária, e para o incentivo à leitura",
+        "Doação ou aquisição de acervos para bibliotecas públicas, museus, arquivos públicos, cinematecas",
+        "Ações de capacitação, treinamento de pessoal, oficinas e aquisição de equipamentos, que tenham como finalidade a manutenção de acervos de bibliotecas públicas, museus, arquivos públicos e cinematecas",
+        "Periódicos e outras publicações",
+        "Festival literário",
+        "Ações de formação e capacitação em geral, incluindo gestão e empreendedorismo cultural",
+        "Construção de bibliotecas desde que esteja prevista a implantação de espaço destinado a apresentações de teatro, exibição de filmes e outras atividades culturais em municípios com menos de 100.000 (cem mil) habitantes")
+
     );
