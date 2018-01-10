74c090c7dc91f23b5c4dd5193c95a904fbdcf496
Gabriel Avaloni <avaloni@users.noreply.github.com>
2018-01-10 14:52:10 -0200

    Corrige validação de preenchimento obrigatório das categorias

--- assets/js/ng.registration.controller.js
+++ assets/js/ng.registration.controller.js
@@ -3,7 +3,7 @@
 
     var module = angular.module('registration.controller', ['ngSanitize']);
 
-    module.controller('RegistrationController',['$scope', 'EditBox', '$http', 'RegistrationService',  function($scope, EditBox, $http, RegistrationService){
+    module.controller('RegistrationController',['$scope', 'EditBox', '$q', '$http', 'RegistrationService',  function($scope, EditBox, $q, $http, RegistrationService){
         var labels = MapasCulturais.gettext.moduleOpportunity;
 
         $scope.createOpportunityRegistration = function() {
@@ -34,58 +34,66 @@
 
 
             var res = registration['category'].match(/Escolha um segmento/g);
+            var defer = $q.defer();
             $('.segment-error-category').remove();
             if(res){
                 var erro = "É necessário selecionar as opções em aberto.";
                 MapasCulturais.Messages.error(labels['correctErrors']);
                 if(jQuery('#segment-error').length === 0){
-                    jQuery('#segment-required').append('<span title="' + erro + '" class="danger hltip js-response-error-category" id="segment-error" data-hltip-classes="hltip-danger"></span>');
+                    jQuery('#segment-required').append('<span title="' + erro + '" class="danger hltip js-response-error js-response-error-category" id="segment-error" data-hltip-classes="hltip-danger"></span>');
                 }
+                defer.reject();
             }else{
 
-                $http.patch('/inscricoes/single/'+MapasCulturais.entity.id, registration).success(function (data, status) {
+                $http.patch('/inscricoes/single/'+MapasCulturais.entity.id, registration).success(() => {
                     MapasCulturais.Messages.success(labels['changesSaved']);
-                }).error(function (data, status) {
+                }).error(() => {
                     MapasCulturais.Messages.error(labels['correctErrors']);
                 });
+
+                defer.resolve();
             }
+
+            return defer.promise;
         };
 
         $scope.sendRegistration = function(){
-            $scope.createOpportunityRegistration();
-            RegistrationService.send($scope.data.entity.id).success(function(response){
-                if(response.error){
-                    var focused = false;
-                    $('.js-response-error').remove();
-                    Object.keys(response.data).forEach(function(field){
-                        var $el;
-                        if(field === 'projectName'){
-                            $el = $('#projectName').parent().find('.label');
-                        }else if(field === 'category'){
-                            $el = $('.js-editable-registrationCategory').parent();
-                        }else if(field.indexOf('agent') !== -1){
-                            $el = $('#' + field).parent().find('.registration-label');
-                        }else {
-                            $el = $('#' + field).find('div:first');
-                        }
+            $scope.createOpportunityRegistration().then(() => {
+                RegistrationService.send($scope.data.entity.id).success(function (response) {
+                    if (response.error) {
+                        var focused = false;
+                        $('.js-response-error').remove();
+                        Object.keys(response.data).forEach(function (field) {
+                            var $el;
+                            if (field === 'projectName') {
+                                $el = $('#projectName').parent().find('.label');
+                            } else if (field === 'category') {
+                                $el = $('.js-editable-registrationCategory').parent();
+                            } else if (field.indexOf('agent') !== -1) {
+                                $el = $('#' + field).parent().find('.registration-label');
+                            } else {
+                                $el = $('#' + field).find('div:first');
+                            }
 
-                        var message = response.data[field] instanceof Array ? response.data[field].join(' ') : response.data[field];
-                        message = message.replace(/"/g, '&quot;');
-                        $scope.data.propLabels.forEach(function(prop){
-                            message = message.replace('{{'+prop.name+'}}', prop.label);
+                            var message = response.data[field] instanceof Array ? response.data[field].join(' ') : response.data[field];
+                            message = message.replace(/"/g, '&quot;');
+                            $scope.data.propLabels.forEach(function (prop) {
+                                message = message.replace('{{' + prop.name + '}}', prop.label);
+                            });
+                            $el.append('<span title="' + message + '" class="danger hltip js-response-error" data-hltip-classes="hltip-danger"></span>');
+                            if (!focused) {
+                                $('html,body').animate({scrollTop: $el.parents('li').get(0).offsetTop - 10}, 300);
+                                focused = true;
+                            }
                         });
-                        $el.append('<span title="' + message + '" class="danger hltip js-response-error" data-hltip-classes="hltip-danger"></span>');
-                        if(!focused){
-                            $('html,body').animate({scrollTop: $el.parents('li').get(0).offsetTop - 10}, 300);
-                            focused = true;
-                        }
-                    });
-                    MapasCulturais.Messages.error('Corrija todos os campos antes de enviar a inscrição.');
-                }else{
-                    MapasCulturais.Messages.success(labels['registrationSent']);
-                    document.location = response.singleUrl;
-                }
-            });
+                        MapasCulturais.Messages.error('Corrija todos os campos antes de enviar a inscrição.');
+                    } else {
+                        MapasCulturais.Messages.success(labels['registrationSent']);
+                        document.location = response.singleUrl;
+                    }
+                });
+            }, () => MapasCulturais.Messages.error('É necessário preencher as áreas de atuação e segmentos, antes de continuar.'));
+
         };
 
     }]);
