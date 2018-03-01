5760d05f88d2cb2ced6008c4ef273a69381d9c8d
Gabriel Avaloni <avaloni@users.noreply.github.com>
2018-03-01 11:52:56 -0300

    Categorias selecionadas pelo usuário não estavam aparecendo na tela de inscrição enviada
    Co-authored-by: Pedro Philipe <pedrophilipeti@gmail.com>

--- layouts/parts/singles/registration-single--categories-sefic.php
+++ layouts/parts/singles/registration-single--categories-sefic.php
@@ -2,10 +2,11 @@
         <div>
             <?php
             $tipologias = require __DIR__ . '/../../../' . 'tipologia-oportunidades.php';
+            $tipologias = call_user_func_array("array_merge", $tipologias);
             $categories = explode(';',$entity->category); ?>
             <h4>Categorias</h4>
-            <?php foreach($tipologias as $category){
-                $segmento = array_search($category, $tipologias);
+            <?php foreach($categories as $category){
+                $segmento = $tipologias[$category];
                 echo $segmento . '</br>';
             }?>
         </div>
